fa70958a9130c88cf9ef0989cc7d5cbb3e436f3c
kholod <kholoddev@gmail.com>
2019-07-29 17:46:21 +0300

    добавлен вывод реестра

--- backend/core/path.php
+++ backend/core/path.php
@@ -1,6 +1,7 @@
 <?php
 // 
 define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/backend/');
+define('HEADERS', ROOT . '/core/headers.php');
 define('DB', ROOT . '/core/db/DB.php');
 define('CLASSES', ROOT . '/classes/');
 define('TRAITS', ROOT . '/classes/traits/');
--- backend/getRegistry.php
+++ backend/getRegistry.php
@@ -1,54 +1,69 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/backend/core/path.php';
+require_once HEADERS;
 require_once API_RENDER;
 require_once CLASSES . 'Users.php';
 require_once CLASSES . 'Contracts.php';
 require_once CLASSES . 'Invoices.php';
 
 $users = new Users();
-$contracts = new Contracts();
-$invoices = new Invoices();
-
-
 $users = $users->getUser();
 
 $registry = [];
 foreach ($users as $user) {
-    $registry[$user['id']] = [
+    $registry[] = [
+        'id' => (int) $user['id'],
         'name' => $user['name'],
-        'contracts' => []
+        'contracts' => parseContracts($user['id'])
     ];
+}
 
-    $userContracts = $contracts->getContract($user['id']);
+sleep(1);
+
+apiRender($registry);
+
+
+
+
+function parseContracts($id)
+{
+    $result = [];
+    $contracts = new Contracts();
+    $userContracts = $contracts->getContract($id);
     foreach ($userContracts as $userContract) {
-        $registry[$user['id']]['contracts'][$userContract['id']] = [
+        $today = date('U');
+        $dateClosure = date('U', strtotime($userContract['date_closure']));
+        $result[] = [
+            'id' => (int) $userContract['id'],
             'name' => $userContract['name'],
             'dateOpening' => $userContract['date_opening'],
             'dateClosure' => $userContract['date_closure'],
-            'status' => 1,
-            'invoices' => []
+            'status' => ($today > $dateClosure) ? 1 : 0,
+            'invoices' => getInvoices($userContract['id'])
         ];
-
-
-        $today = date('U');
-        $dateClosure = date('U', strtotime($userContract['date_closure']));
-        if($today > $dateClosure) {
-            $registry[$user['id']]['contracts'][$userContract['id']]['status'] = 0;
-        }
-       
-
-        $contractInvoices = $invoices->getInvoice(NULL, $userContract['id']);
-        foreach ($contractInvoices as $contractInvoice) {
-            $registry[$user['id']]['contracts'][$userContract['id']]['invoices'][$contractInvoice['id']] = [
-                'date' => $contractInvoice['date'],
-                'amount' =>  $contractInvoice['amount']
-            ];
-        }
     }
 
+    return $result;
 }
 
-sleep(3);
 
-apiRender($registry);
+function getInvoices($id)
+{
+    $result = [];
+    $invoices = new Invoices();
+    $invoices = $invoices->getInvoice(NULL, $id);
+    foreach ($invoices as $invoice) {
+        $result[] = [
+            'id'     => $invoice['id'],
+            'date'   => $invoice['date'],
+            'amount' => $invoice['amount']
+        ];
+    }
+
+    return $result; 
+}
+
+
+
+
