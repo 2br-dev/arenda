4d03419e0dca9903a5c8e34cbd4003165eecb324
kholod <kholoddev@gmail.com>
2019-08-02 15:52:15 +0300

    добавил возможность добавить договор

--- frontend-web/src/components/Contracts.js
+++ frontend-web/src/components/Contracts.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { getContracts } from './../Update'
+
 import ContractsView from './views/ContractsView'
 
 import { BACKEND } from './../path.js'
@@ -19,16 +21,97 @@ class Contracts extends React.Component {
 
     addSubmit(e) {
         e.preventDefault()
-        console.log('submit contracts');
         
+        let errors = []
+        let isValid = true
+        
+        const userId      = document.getElementById('addSelect').value
+        const name        = document.getElementById('addName').value.trim()
+        const price    = parseFloat(document.getElementById('addPrice').value.trim())
+        const discount = parseFloat(document.getElementById('addDiscount').value.trim())
+        
+        let dateTo   = document.getElementById('addDateTo').value.trim().split('-')
+        dateTo = `${dateTo[2]}-${dateTo[1]}-${dateTo[0]}`
+        
+        let dateFrom = document.getElementById('addDateFrom').value.trim().split('-')
+        dateFrom = `${dateFrom[2]}-${dateFrom[1]}-${dateFrom[0]}`
+        
+        
+        if(userId === '') {
+            errors.push('Не указан пользователь')
+            isValid = false
+        }
+        if(name === '') {
+            errors.push('Не указано наименование')
+            isValid = false
+        }
+        if(dateTo === '') {
+            errors.push('Не указана дата От')
+            isValid = false
+        }
+        if(dateFrom === '') {
+            errors.push('Не указана дата До')
+            isValid = false
+        }
+        if(isNaN(price)) {
+            errors.push('Не корректная стоимость за расчетный период')
+            isValid = false
+        }
+        if(isNaN(discount)) {
+            errors.push('Не корректный процент дисконта')
+            isValid = false
+        }       
+        
+        if(errors.length > 0) {
+            this.setState({addError: errors.join(' | ')})
+        } else {
+            this.setState({addError: ''})
+        }
+        
+        const Data = new FormData()
+        Data.append('user_id', userId)
+        Data.append('name', name)
+        Data.append('date_to', dateTo)
+        Data.append('date_from', dateFrom)
+        Data.append('price', price)
+        Data.append('discount', discount)
+        
+        if(isValid) {
+            const URL = `${BACKEND}addContract`
+            const OPTIONS = {
+                mode: 'cors',
+                method: 'POST',
+                body: Data
+            }
+
+            fetch(URL, OPTIONS)
+                .then(response => response.json())
+                .then(response => {
+                    if(response === 1) {
+                        this.setState({addError: ''})
+                        this.update()
+                    } else {
+                        this.setState({addError: 'Договор с таким наименованием уже существует у данного пользователя'})
+                    }
+                })   
+        }
     }
+
+
+    update() {
+        getContracts().then(resolve => {
+            this.props.onUpdateContracts(resolve)
+        })
+    }
+
         
     render() {
         return (
             <ContractsView
                 users={this.props.users}
                 contracts={this.props.contracts}
-                update={this.state.update}
+                addSubmit={this.addSubmit.bind(this)}
+                addError={this.state.addError}
             />
         )
     }
@@ -40,6 +123,10 @@ const mapStateToProps = state => ({
     contracts: state.dashboard.contracts
 })
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    onUpdateContracts: contracts => {
+        dispatch({ type: 'UPDATE_CONTRACTS', payload: contracts })
+    }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Contracts)
--- frontend-web/src/components/views/ContractsView.js
+++ frontend-web/src/components/views/ContractsView.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState }from 'react'
 
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
 
 import './../../assets/css/Contracts.css'
 
@@ -8,7 +10,11 @@ const ContractsView = props => {
     return (
         <div className="content">
             <div className="content__body __center">
-                <Add/>
+                <Add 
+                    users={props.users} 
+                    submit={props.addSubmit} 
+                    error={props.addError}
+                />
                 <List contracts={props.contracts}/>
             </div>
         </div>
@@ -19,21 +25,80 @@ const ContractsView = props => {
 export default ContractsView
 
 
-const Add = (props) => {
+const Add = ({users, submit, error}) => {
+
+    const to = new Date()
+    const from = new Date(to.getFullYear() + 1, to.getMonth(), to.getDate())
+    const [dateTo, setDateTo] = useState(to)
+    const [dateFrom, setDateFrom] = useState(from)
+    
+    
+    const addErrorClass = addError(error)
+
+
     return (
-        <p></p>
+        <form className="add" onSubmit={submit}>
+            <div className="add__box">
+                <label htmlFor="addSelect" className="add__label">Выберете пользователя из списка:</label>
+                <select id="addSelect" className="add__select">
+                    {users.map(user =>
+                        <option key={`user_${user.id}`} className="add__select_option" value={user.id}>{user.name}</option>
+                    )}
+                </select>
+            </div>
+            <div className="add__box">
+                <input id="addName" type="text" className="add__input" placeholder="Наименование договора*"/>
+            </div>
+            <div className="add__box">
+                <p className="add__label">Даты начала и окончания действия договора</p>
+                <div className="add__box-date">
+                    <DatePicker
+                        selected={dateTo}
+                        onChange={setDateTo}
+                        className='add__input-date'
+                        dateFormat="dd-MM-yyyy"
+                        id="addDateTo"
+                    />
+                    <DatePicker
+                        selected={dateFrom}
+                        onChange={setDateFrom}
+                        minDate={dateTo}
+                        dateFormat="dd-MM-yyyy"
+                        className='add__input-date'
+                        id="addDateFrom"
+                    />
+                </div>
+            </div>
+            <div className="add__box">
+                <input id="addPrice" type="text" className="add__input" placeholder="Стоимость за расчетный период*"/>
+            </div>
+            <div className="add__box">
+                <input id="addDiscount" type="text" className="add__input" placeholder='Процент дисконта (от 0 до 100, дробь через < . >)*'/>
+            </div>
+            <div className="add__box-submit">
+                <button className="add__submit">Добавить договор</button>
+            </div>
+            <p className={addErrorClass}>{error}</p>
+        </form>
     )
 }
 
 
 const List = (props) => {
-    
     return (
         <section className="list">
             <p className="list__title">Список договоров</p>
             {props.contracts.map(contract => 
-                <a className="list__link">{contract.name}</a>
+                <a key={`contract_${contract.id}`} className="list__link">{contract.name}</a>
             )}
         </section>
     )
 }
+
+function addError(value) {
+    let temp = 'add__error '
+    if(value !== '') {
+        temp += 'add__error-visible'
+    }
+    return temp
+} 
